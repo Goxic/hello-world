173897aa485871f0f849476b8d47528b64e98c4d
goxic <947116910@qq.com>
2018-12-05 21:50:23 +0800

    homework

--- equation.c
+++ equation.c
@@ -2,3 +2,25 @@
 //ax + by = c
 //dx + ey = f
 //注意输入/输出，数据类型
+#include<stdio.h>
+main()
+{
+ float a,b,c,d,e,f,x,y;
+ printf("a,b,c\n");
+ scanf("%f%f%f",&a,&b,&c);
+ printf("d,e,f\n");
+ scanf("%f%f%f",&d,&e,&f);
+  if((a==0&&b==0&&c!=0)||(d==0&&e==0&&f!=0))
+  printf("no answer");
+  else
+  {
+   if(a*f*e==b*d*f==c*e*d)
+   printf("infinite answer");
+   else
+   {
+    x=(c*e-f*b)/(a*e-d*b);
+    y=(c*d-f*a)/(d*b-a*e);
+    printf("x=%.2f,y=%.2f",x,y);
+   }
+  }
+}
